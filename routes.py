b9b9476e8e07b9f608cf9558aed9e67f38dead48
Alhussiny <eng.elhoseny.osama@gmail.com>
2019-03-03 19:18:07 +0200

    disable debug

--- routes.py
+++ routes.py
@@ -561,5 +561,6 @@ def is_admin():
         return False
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port='5000')
+    #app.debug = True
+    #app.run(host='0.0.0.0', port='5000')
+    app.run()
