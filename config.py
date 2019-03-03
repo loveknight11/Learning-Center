75c461d418388b1a42a3fd140f77b7944ab6eca8
Alhussiny <eng.elhoseny.osama@gmail.com>
2019-03-03 19:00:43 +0200

    deploy to heroku

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config(object):
     SECRET_KEY = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'db.db')+'?check_same_thread=False'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
