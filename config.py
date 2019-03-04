84acebe216b6c3baef9bd2c827853eee43cf0c4a
Alhussiny <eng.elhoseny.osama@gmail.com>
2019-03-04 16:56:31 +0200

    change secret key

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+    SECRET_KEY = 'a-very-hard-word-to-guess-for-me-and-all-habeba-kenzy'
+    #SECRET_KEY = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'db.db')+'?check_same_thread=False'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
